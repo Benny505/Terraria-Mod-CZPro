ee04d4dfcf95f94f2e2bfd31f2d04f983ca5d0a9
callum mitchell <callumlovesbooks@icloud.com>
2021-04-09 11:38:34 -0600

    car and shoes

---
+++ Items/ChubbyKey.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CZPro.Mounts;
+
+namespace CZPro.Items
+{
+    class ChubbyKey : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Chubby Key");
+            Tooltip.SetDefault("Spawn the chubby vehicle");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.value = 76432;
+            item.rare = ItemRarityID.Yellow;
+            item.noMelee = true;
+            item.mountType = ModContent.MountType<ChubbyVehicle>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock, 1);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Mounts/ChubbyVehicle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CZPro.Buffs;
+using Microsoft.Xna.Framework;
+
+namespace CZPro.Mounts
+{
+	class ChubbyVehicle : ModMountData
+	{
+		public override void SetDefaults()
+		{
+			//basic data
+			mountData.buff = ModContent.BuffType<ChubbyMount>();
+			mountData.spawnDust = 16;
+			mountData.heightBoost = 20;
+			mountData.fallDamage = 0;
+			mountData.runSpeed = 30f;
+			mountData.dashSpeed = 10f;
+			mountData.flightTimeMax = 0;
+			mountData.fatigueMax = 0;
+			mountData.jumpHeight = 1;
+			mountData.acceleration = 0.3f;
+			mountData.jumpSpeed = 6f;
+			mountData.blockExtraJumps = true;
+			mountData.constantJump = true;
+
+			//how many frames are in the animation
+			mountData.totalFrames = 4;
+
+			//tells the game how to position the player in referance to the entire texture
+			int[] array = new int[mountData.totalFrames];
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = 20;
+			}
+			mountData.playerYOffsets = array;
+			mountData.xOffset = 13;
+			mountData.bodyFrame = 3;
+			mountData.yOffset = -12;
+			mountData.playerHeadOffset = 22;
+
+			//////animation data///////
+			//animation when still
+			mountData.standingFrameCount = 1;
+			//animation when moving
+			mountData.runningFrameCount = 4;
+			mountData.runningFrameDelay = 30;
+			mountData.runningFrameStart = 0;
+			//animation when in the air
+			mountData.inAirFrameCount = 1;
+			mountData.inAirFrameDelay = 40;
+			mountData.inAirFrameStart = 0;
+			//animation when swimming
+			mountData.swimFrameCount = 4;
+			mountData.swimFrameDelay = 70;
+			mountData.swimFrameStart = 0;
+
+			//specifying the texture size for animations
+			mountData.textureWidth = mountData.backTexture.Width + 20;
+			mountData.textureHeight = mountData.backTexture.Height;
+		}
+
+    }
+}
